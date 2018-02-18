a794a3cc4bc6db7fb40a9a88da14c3066b22a53a
Khoa Le <le.p.khoa@gmail.com>
2018-02-17 20:50:24 -0700

    Linked list problem 3 SOLVED.

--- Chapter2-LinkedLists/Q2-3.py
+++ Chapter2-LinkedLists/Q2-3.py
@@ -1,3 +1,5 @@
+from linkedlist import Node
+
 def del_middle_node(mNode):
     currentNode = mNode
 
@@ -7,3 +9,28 @@ def del_middle_node(mNode):
 
     currentNode.data = currentNode.next.data
     currentNode.next = None
+
+def getNode(head, targetNode):
+    currentNode = head
+    while currentNode is not None:
+        if currentNode.data == targetNode:
+            return currentNode
+        currentNode = currentNode.next
+
+    return None
+
+testLinkedlist = Node(1)
+testLinkedlist.createLinkedListFromList([2,3,4,5])
+targetNode = getNode(testLinkedlist, 3)
+
+del_middle_node(targetNode)
+
+testLinkedlist.printLinkedList()
+
+testLinkedlist2 = Node(1)
+testLinkedlist2.createLinkedListFromList([2,3,9,5])
+targetNode2 = getNode(testLinkedlist2, 9)
+
+del_middle_node(targetNode2)
+
+testLinkedlist2.printLinkedList()
