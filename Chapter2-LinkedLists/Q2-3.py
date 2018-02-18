346033f0e5607098e0c2ed02faa09ac09aae41ec
Khoa Le <le.p.khoa@gmail.com>
2018-02-18 00:31:35 -0700

    Much faster version. Realize that we need to look at the linked list as a whole.

--- Chapter2-LinkedLists/Q2-3.py
+++ Chapter2-LinkedLists/Q2-3.py
@@ -19,11 +19,25 @@ def getNode(head, targetNode):
 
     return None
 
+#much better version
+#remember that deleting a node is just rearranging its next pointer to skip the one we want to delete
+#also I don't have to shift the whole linked list
+#finally and most importantly, look at the linked list as a whole. Realize that what we were trying to achieve didn't require shifting the whole array
+# ex. 1->2->3->4->5 and we want to delete 3 then we would get 1->2->4->5
+# from this example we should realize that the 4 is in the old place of the three!
+def better_del_middle_node(mNode):
+    if mNode is None or mNode.data == None:
+        return False
+
+    mNode.data = mNode.next.data
+    mNode.next = mNode.next.next
+    return True
+
 testLinkedlist = Node(1)
 testLinkedlist.createLinkedListFromList([2,3,4,5])
 targetNode = getNode(testLinkedlist, 3)
 
-del_middle_node(targetNode)
+better_del_middle_node(targetNode)
 
 testLinkedlist.printLinkedList()
 
@@ -31,6 +45,6 @@ testLinkedlist2 = Node(1)
 testLinkedlist2.createLinkedListFromList([2,3,9,5])
 targetNode2 = getNode(testLinkedlist2, 9)
 
-del_middle_node(targetNode2)
+better_del_middle_node(targetNode2)
 
 testLinkedlist2.printLinkedList()
